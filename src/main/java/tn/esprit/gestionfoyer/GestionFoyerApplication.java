6d594c2d08a74de4d4437a1863b7b9f769a6e3be
skander-bedwi <74327661+skander-bedwi@users.noreply.github.com>
2023-11-20 23:43:57 +0100

    first commit

---
+++ src/main/java/tn/esprit/gestionfoyer/GestionFoyerApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.gestionfoyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFoyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFoyerApplication.class, args);
+	}
+
+}
